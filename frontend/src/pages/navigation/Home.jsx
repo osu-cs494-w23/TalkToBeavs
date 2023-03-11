909254ac02043bfcd18fa007d3a5175cfa15c6b4
Artem Kolpakov <artemkolpakov78@gmail.com>
2023-03-10 19:26:52 -0800

    Added the feed page and logic for the downvoting/upvoting

--- frontend/src/pages/navigation/Home.jsx
+++ frontend/src/pages/navigation/Home.jsx
@@ -1,68 +1,65 @@
 import React from "react";
-import {
-  Text,
-  Heading,
-  Flex,
-  Box,
-} from "@chakra-ui/react";
+import Post from "../../components/Post";
+import { Text, Heading, Flex, Box } from "@chakra-ui/react";
 
-const posts = 
-[
-	{
-		_id: 0,
-		content: "This is my first post",
-		rating: 0,
-		postedBy: "artem"
-	},
-	{
-		_id: 1,
-		content: "This is my first post",
-		rating: 3,
-		postedBy: "colby"
-	},
-	{
-		_id: 2,
-		content: "This is my first post",
-		rating: 1,
-		postedBy: "tom"
-	},
-	{
-		_id: 3,
-		content: "This is my first post",
-		rating: 2,
-		postedBy: "jeff"
-	},
-	{
-		_id: 4,
-		content: "This is my second post",
-		rating: 1,
-		postedBy: "colby"
-	},
-	{
-		_id: 5,
-		content: "This is my third post",
-		rating: 0,
-		postedBy: "colby"
-	},
-	{
-		_id: 6,
-		content: "This is my second post",
-		rating: 0,
-		postedBy: "artem"
-	},
-]
+import { setAllPosts, selectAllPosts } from "../../redux/slices/FeedSlice";
 
-function Home()
-{
-	
-	
-	return (
-        <Flex w="100%" direction="column" justify="center" pt={150}>
-			{posts.map(post => (
-				<Post key={post._id} post={post} />
-			))}
-		</Flex>
-	)
+//TODO: fetch this data from the store/MongoDB using setAllPosts to fetch all the existing post data and
+// selectAllPosts to get it from the store. We need to figure out whether we want to do the business MongoDB logic
+// within the store or here within the component
+const posts = [
+  {
+    _id: 0,
+    content: "This is my first post",
+    rating: 0,
+    postedBy: "artem",
+    createdAt: "1/1/2023",
+  },
+  {
+    _id: 1,
+    content: "This is my first post",
+    rating: 3,
+    postedBy: "colby",
+    createdAt: "1/1/2023",
+  },
+  {
+    _id: 1,
+    content: "This is my first post",
+    rating: 3,
+    postedBy: "colby",
+    createdAt: "1/1/2023",
+  },
+  {
+    _id: 1,
+    content: "This is my first post",
+    rating: 3,
+    postedBy: "colby",
+    createdAt: "1/1/2023",
+  },
+  {
+    _id: 1,
+    content: "This is my first post",
+    rating: 3,
+    postedBy: "colby",
+    createdAt: "1/1/2023",
+  },
+  {
+    _id: 1,
+    content: "This is my first post",
+    rating: 3,
+    postedBy: "colby",
+    createdAt: "1/1/2023",
+  },
+];
+
+function Home() {
+  return (
+    <Flex w="100%" direction="column" justify="center" pt={150}>
+      {posts.map((post) => (
+        <Post key={post._id} post={post} />
+      ))}
+    </Flex>
+  );
 }
 
 export default Home;
--- frontend/src/redux/slices/FeedSlice.js
+++ frontend/src/redux/slices/FeedSlice.js
@@ -11,8 +11,30 @@ const feedSlice = createSlice({
     addPost: (state, action) => {
       state.posts.push(action.payload);
     },
+    setAllPosts: (state, action) => {
+      state.posts = action.payload;
+    },
+    upvotePost: (state, action) => {
+      const index = state.posts.findIndex((post) => post._id === action.payload._id);
+      if (index !== -1) {
+        state.posts[index].rating += 1;
+        // TODO:
+        // update the state.posts[index].rating += 1 in the MongoDB right here
+      }
+    },
+    downvotePost: (state, action) => {
+      const index = state.posts.findIndex((post) => post._id === action.payload._id);
+      if (index !== -1) {
+        state.posts[index].rating -= 1;
+        // TODO:
+        // update the state.posts[index].rating -= 1 in the MongoDB right here
+      }
+    },
   },
 });
 
-export const { addPost } = feedSlice.actions;
+export const { addPost, setAllPosts, upvotePost, downvotePost } = feedSlice.actions;
+
+export const selectAllPosts = (state) => state.posts
+
 export default feedSlice.reducer;
